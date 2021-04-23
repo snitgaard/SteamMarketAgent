a3625c28d8dae67c827c3399fea8d2527588bcd1
dreamy <31621059+dreamyy1@users.noreply.github.com>
2021-04-23 10:50:15 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SteamMarketAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
