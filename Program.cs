f49bf5d2a1290ab32b2a5f32ef7dcc91708c3244
dreamy <31621059+dreamyy1@users.noreply.github.com>
2021-05-07 13:01:16 +0200

    initial commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace SteamMarketAgent
 {
@@ -6,7 +11,34 @@ namespace SteamMarketAgent
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            startCrawlerAsync();
+            Console.ReadLine();
+        }
+
+        private static async Task startCrawlerAsync()
+        {
+            var skins = new List<Skin>();
+            var url =
+                "https://steamcommunity.com/market/listings/730/Desert%20Eagle%20%7C%20Printstream%20%28Minimal%20Wear%29";
+            var httpClient = new HttpClient();
+            var html = await httpClient.GetStringAsync(url);
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(html);
+            var divs = htmlDocument.DocumentNode.Descendants("div").Where(node => node.GetAttributeValue("class", "")
+                .Equals("market_listing_row market_recent_listing_row listing")).ToList();
+
+            foreach (var div in divs)
+            {
+                var skin = new Skin
+                {
+                    SkinModel = div.Descendants("span").FirstOrDefault().ChildAttributes("span").FirstOrDefault().Value,
+                    Price = div.Descendants("span").FirstOrDefault(node => node.GetAttributeValue("class", "")
+                        .Equals("market_listing_price market_listing_price_with_fee")).InnerText,
+                };
+                skins.Add(skin);
+                Console.WriteLine(skin.Price);
+            }
+
         }
     }
 }
---
+++ Skin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SteamMarketAgent
+{
+    public class Skin
+    {
+        public string SkinModel { get; set; }
+        public string Price { get; set; }
+        public string Link { get; set; }
+
+    }
+}
