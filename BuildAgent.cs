5e1cef5c51e5c666091763f01f6df18a436222de
The Joker <54842770+Nbruunj@users.noreply.github.com>
2021-05-25 09:55:24 +0200

    bent ting

--- BuildAgent.cs
+++ BuildAgent.cs
@@ -1,10 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace SteamMarketAgent
 {
     class BuildAgent
     {
+        public string Id { get; private set; }
+        public string BuildPath { get; private set; }
+
+        public int TimeIntervalSeconds { get; set; }
+
+        private object _agentLock;
+
+        private readonly Thread _t;
+        private bool _done;
+
+        public BuildAgent(string id, string solutionPath, int seconds, object agentLock)
+        {
+            Id = id;
+            BuildPath = solutionPath;
+            TimeIntervalSeconds = seconds;
+            _agentLock = agentLock;
+            _done = false;
+
+            _t = new Thread(Run);
+            _t.Start();
+        }
+
+        public void Run()
+        {
+            while (!_done)
+            {
+                try
+                {
+                    lock (_agentLock)
+                    {
+                        var exitCode = Do("build", BuildPath);
+                        if (exitCode == 0)
+                            Do("test", BuildPath);
+                    }
+                    Thread.Sleep(TimeIntervalSeconds * 1000);
+                }
+                catch (ThreadInterruptedException)
+                {
+                    Console.WriteLine("Thread interrupted");
+                    // just wake up
+                }
+            }
+            Console.WriteLine("Agent stopped");
+        }
+
+        private int Do(string dotnetCommand, string buildPath)
+        {
+            Console.WriteLine($"{Id} {dotnetCommand}...");
+            var p = new Process
+            {
+                StartInfo =
+                {
+                    UseShellExecute = false,
+                    RedirectStandardOutput = true,
+                    FileName = "dotnet",
+                    WorkingDirectory = buildPath,
+                    Arguments = dotnetCommand
+                }
+            };
+            Console.WriteLine("Start process...");
+            p.Start();
+            p.WaitForExit();
+            Console.WriteLine("Process terminated");
+
+            Console.WriteLine("Writing to log file...");
+            string logPath = buildPath + @"\logs\";
+            if (!Directory.Exists(logPath))
+            {
+                Directory.CreateDirectory(logPath);
+            }
+            using (var output = File.AppendText(logPath + $"{Id}.log"))
+            {
+                output.WriteLine($"Time: {p.StartTime}");
+                output.WriteLine(p.StandardOutput.ReadToEnd());
+                output.WriteLine();
+            }
+            var exitCode = p.ExitCode;
+            p.Close();
+            p.Dispose();
+            Console.WriteLine("Done");
+            return exitCode;
+        }
+
+        public void Cancel()
+        {
+            _done = true;
+            if (_t.ThreadState != System.Threading.ThreadState.Running)
+                _t.Interrupt();
+        }
     }
 }
--- MyBuildManager.cs
+++ MyBuildManager.cs
@@ -6,5 +6,57 @@ namespace SteamMarketAgent
 {
     class MyBuildManager
     {
+        private Dictionary<string, BuildAgent> agents = new Dictionary<string, BuildAgent>();
+        private Dictionary<string, int> agentLocks = new Dictionary<string, int>();
+
+        public void AddAgent(string agentId, string solutionPath, int intervalSeconds)
+        {
+            if (agents.ContainsKey(agentId))
+            {
+                throw new ArgumentException("Agent id already in use [{0}]", agentId);
+            }
+
+            // all locks on build paths are in lowercase.
+            solutionPath = solutionPath.ToLower();
+
+            // get the lock for this build path. If no lock exists then create one
+            // note: ints are used as lock objects, so we can count the number of agents
+            // using it.
+            if (!agentLocks.ContainsKey(solutionPath))
+            {
+                agentLocks.Add(solutionPath, 0);
+            }
+
+            // increasing the lock count since we now have one more agent.
+            var agentLock = ++agentLocks[solutionPath];
+
+            // creating the agent with the lock object
+            agents[agentId] = new BuildAgent(agentId, solutionPath, intervalSeconds, agentLock);
+        }
+
+        public void RemoveAgent(string agentId)
+        {
+            // is the agentID valid ?
+            if (!agents.ContainsKey(agentId))
+            {
+                throw new ArgumentException("Unknown Agent id [{0}]", agentId);
+            }
+
+            // cancel the tread for this agent
+            BuildAgent agent = agents[agentId];
+            agent.Cancel();
+
+            // decreasing the agent count for this build path
+            var agentLock = --agentLocks[agent.BuildPath.ToLower()];
+
+            // if no agent on this build path, then remove the lock for this build path
+            if (agentLock == 0)
+            {
+                agentLocks.Remove(agent.BuildPath.ToLower());
+            }
+
+            // and of course remove the agent it selv from the manager.
+            agents.Remove(agentId);
+        }
     }
 }
